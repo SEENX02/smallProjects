b47b078fb261f0ec46075acafa2af1145a2bc358
SEENX02 <166158610+SEENX02@users.noreply.github.com>
2025-09-09 12:07:35 +0530

    Delete verification.py

--- verification.py
+++
@@ -1,8 +0,0 @@
-import dbConfig
-
-def checker(pin: int):
-    account = dbConfig.fetch_account_by_pin(pin)
-    return account is not None
-
-def accountDetailsChecker(pin: int):
-    return dbConfig.fetch_account_by_pin(pin)
